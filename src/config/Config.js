ba33fa02eedeb2bd22e2fc9ab61697c792890b1d
phudnguyen <p.nguyen.1@elsevier.com>
2018-04-11 02:11:29 +0700

    added v2 config

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,6 +1,6 @@
 const apiConfig = {
-  //baseUrl: 'https://gm-java-backend.cfapps.io/api',
-  baseUrl: 'http://192.168.1.8:9876/api',
+  baseUrl: 'https://gm-node-backend-hyperlogical-woodhouse.cfapps.io/api',
+  //baseUrl: 'http://192.168.1.8:9876/api',
   facebookAppId: '568365100192445',
   loginRedirectUrl: 'http://localhost:3000/pfm/login',
   unauthorizedPath: '/gm/unauthorized',
